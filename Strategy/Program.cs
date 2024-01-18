55e83d2f9ec2800cc5ac16be26a2f99a3b25d342
Rasmus Rimestad <rasmus.rimestad@cloudberries.no>
2024-01-18 10:37:49 +0100

    Add example policy

--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Strategy;
+
+var policy = new Policy
+{
+    Age = 18,
+    InterestRate = 0.05f,
+    Memberships = new List<string> { "LO" }
+};
+
+var engine = new PolicyRuleEngine();
+
+Console.WriteLine("Policy before parsing: " + policy);
+engine.ParsePolicy(policy);
+
+Console.WriteLine("Policy after parsing: " + policy);
